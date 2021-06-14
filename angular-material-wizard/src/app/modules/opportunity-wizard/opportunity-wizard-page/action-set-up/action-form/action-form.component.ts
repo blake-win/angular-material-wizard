6876b7806ee259d49dfa64ae182e8f9b2cbc32fd
travis.ht216 <travis.ht216@gmail.com>
2021-06-14 13:23:17 -0400

    feat: get custom fields in action setup form

--- angular-material-wizard/src/app/modules/opportunity-wizard/opportunity-wizard-page/action-set-up/action-form/action-form.component.ts
+++ angular-material-wizard/src/app/modules/opportunity-wizard/opportunity-wizard-page/action-set-up/action-form/action-form.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../../../../../store/app.reducer';
+import { map } from 'rxjs/operators';
+import { Field } from '../../field-set-up/store/field.model';
 
 @Component({
   selector: 'app-action-form',
   templateUrl: './action-form.component.html',
   styleUrls: ['./action-form.component.scss']
 })
-export class ActionFormComponent implements OnInit {
+export class ActionFormComponent implements OnInit, OnDestroy {
 
-  actionForm: FormGroup
+  actionForm: FormGroup;
+  fieldList: Field[];
+
+  private subscription: Subscription;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -18,6 +24,25 @@ export class ActionFormComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.getCustomFields();
+    this.initializeForm();
+  }
+
+  addAction(): void {
+    console.log(this.actionForm.value);
+    // Dispatch Add action here
+  }
+
+  private getCustomFields(): void {
+    this.subscription = this.store.select('fields')
+      .pipe(map(fieldsState => fieldsState.fields))
+      .subscribe((fields: Field[]) => {
+        this.fieldList = fields;
+        console.log(this.fieldList);
+      })
+  }
+
+  private initializeForm(): void {
     this.actionForm = this.formBuilder.group({
       actionName: ['', Validators.required],
       stage: ['', Validators.required],
@@ -28,9 +53,8 @@ export class ActionFormComponent implements OnInit {
     })
   }
 
-  addAction(): void {
-    console.log(this.actionForm.value);
-    // Dispatch Add action here
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
